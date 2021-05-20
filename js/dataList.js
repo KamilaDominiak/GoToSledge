45d92a4460cc16808c8fa94283d6935fb132c918
KamilaDominiak <dominiak.kamila@tlen.pl>
2021-05-20 22:12:56 +0200

    Big changes - Kamila will try to make commits every day when she works

---
+++ js/dataList.js
@@ -0,0 +1,17 @@
+//show points in map
+const dataList = [
+  {
+    id: 0,
+    name: "Górka przy nałkoskiej",
+    city: "Gdynia",
+    district: "Karwiny",
+    position: { lat: 54.471952, lng: 18.501946 },
+  },
+  {
+    id: 1,
+    name: "Górka przy Fizylierów",
+    city: "Gdynia",
+    district: "Karwiny",
+    position: { lat: 54.476191, lng: 18.501077 },
+  },
+];
--- js/javaScriptmap.js
+++ js/javaScriptmap.js
@@ -1,21 +1,14 @@
-//show points in map
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map2"), {
+//     zoom: 15,
+//     center: dataList[0].position,
+//   });
 
-function initMap() {
-  const myLatLng = { lat: 54.471952, lng: 18.501946  };
-  const myLatLng2 = { lat: 54.476191, lng: 18.501077 };
-  const map = new google.maps.Map(document.getElementById("map2"), {
-    zoom: 15,
-    center: myLatLng,
-  });
-
-  new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "przy Nałkowskiej",
-    });
-    new google.maps.Marker({
-      position: myLatLng2,
-      map,
-      title: "Fizylierów",
-    });
-}
+//   for (var i = 0; i < dataList.length; i++) {
+//     new google.maps.Marker({
+//       position: dataList[i].position,
+//       map,
+//       title: dataList[i].name,
+//     });
+//   }
+// }
